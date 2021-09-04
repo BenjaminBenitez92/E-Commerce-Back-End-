f9c739bc91ca2a09f47565754e5e3fd99cec8251
BenjaminBenitez92 <81438491+BenjaminBenitez92@users.noreply.github.com>
2021-09-04 14:41:17 -0400

    done with all the routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -33,7 +33,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
@@ -42,7 +42,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
     const userData = await Category.update(req.body, {
       where: {
@@ -60,7 +60,7 @@ router.put('/:id', (req, res) => {
 });
 
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
     const categoryData = await Category.destroy({
       where: {
